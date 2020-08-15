ebf8a04c3f14032c653beeca55083ee92694736a
Katerina <matveevakaterinka@gmail.com>
2020-08-16 00:13:13 +1000

    copy msg animation added

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -16,14 +16,17 @@ class ColorBox extends React.Component {
   render() {
     const { name, background } = this.props
     const { copied } = this.state
+    const overlayClassName = copied ? 'copy-overlay show' : 'copy-overlay'
+    const copyMsgClassName = copied ? 'copy-msg show' : 'copy-msg'
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="colorBox">
-          <div
-            style={{ background }}
-            className={`copy-overlay ${copied && 'show'}`}
-          />
+          <div style={{ background }} className={overlayClassName} />
+          <div className={copyMsgClassName}>
+            <h1>copied</h1>
+            <p>{background}</p>
+          </div>
           <div className="copy-container">
             <div className="box-content">{name}</div>
             <button className="copy-btn">copy</button>
